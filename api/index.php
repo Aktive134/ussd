cee7a43cb9c2b2ac08f20e060005db50d3788c16
aktive134 <reiymondjon134@gmail.com>
2023-11-06 12:59:26 +0100

    modified index, menu files, included readMe

--- api/index.php
+++ api/index.php
@@ -3,9 +3,9 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require '../../../include/utlites/menu.php';
-require_once '../../../include/utlites/util.php';
-require '../../../include/dbsol/conn.php';
+require '../include/utilities/menu.php';
+require_once '../include/utilities/util.php';
+require '../include/dbsol/conn.php';
 require '../vendor/autoload.php';
 
 $app = AppFactory::create();
@@ -18,7 +18,7 @@ $app->post('/', function (Request $request, Response $response) {
     $phone = $_POST['phoneNumber'];
     $text = $_POST['text'];
 
-    $isRegistered = false;
+    $isRegistered = true;
 
     // Create an object instance of the class Menu
     $menu = new Menu($text, $sessionId);
@@ -28,24 +28,43 @@ $app->post('/', function (Request $request, Response $response) {
         if ($text == '' && !$isRegistered) {
             // User is registered and string is empty
             $menu->mainMenuRegistered();
+
         } elseif ($text == '' && $isRegistered) {
             // User is unregistered and string is empty
             $menu->mainMenuUnRegistered();
+
         } elseif ($text !== '' && $isRegistered) {
             // User is unregistered and string is not empty
             $textArray = explode('*', $text);
             switch ($textArray[0]) {
                 case 1:
-                    $menu->subMenuUnRegisteredOne();
+                    $menu->registerMenu($textArray);
                     break;
                 case 2:
                     $menu->subMenuUnRegisteredTwo();
                     break;
                 default:
-                    echo 'END Invalid choice. Thanks for using Bivety Bank';
+                    echo 'END Invalid choice. Please try again. Thanks for using Bivety Bank';
             }
         } elseif ($text !== '' && !$isRegistered) {
             // User is registered and string is not empty
+            $textArray = explode('*', $text);
+
+            switch ($textArray[0]) {
+                case 1:
+                    $menu->sendMoneyMenu($textArray);
+                    break;
+                case 2:
+                    $menu->withdrawMoneyMenu($textArray);
+                    break;
+                case 3:
+                    $menu->checkBalanceMenu($textArray);
+                    break;  
+                default:
+                    echo 'END Invalid choice. Please try again. Thanks for using Bivety Bank';
+                    break;
+            } 
+
         }
     } catch (PDOException $e) {
         $failed = [
--- include/utilities/menu.php
+++ include/utilities/menu.php
@@ -1,8 +1,6 @@
 <?php
-    require_once '../../../include/utlites/util.php';
-    
-
-    class Menu {
+    require_once '../include/utilities/util.php';
+        class Menu {
         protected $text;
         protected $sessionId;
 
@@ -12,20 +10,46 @@
             $message = "CON Welcome to Bivety Bank. Would you like to: " .
             "\n1. Send Money" .
             "\n2. Withdraw Money" .
-            "\n3. Add to Wallet";
+            "\n3. Check Balance";
 
             echo $message;
         }
         public function mainMenuUnRegistered(){
-            $message = "CON Welcome to Bivety Bank .Unfortunately, we can't see your number in our system. Would you like to register? " .
+            $message = "CON Welcome to Bivety Bank .Unfortunately, we can't see your number in our system. Would you like to Register? " .
 			"\n1. Yes" .
 			"\n2. No";
             echo $message;
         }
 
-        public function subMenuUnRegisteredOne(){
-            $message =  "END Your number has been recorded, we would get back to you";
-            echo $message;
+        public function registerMenu($textArray){
+            $level = count($textArray);
+
+            if($level == 1) {
+                echo 'CON Please enter your Full Name:';
+
+            } else if ($level == 2) {
+                echo 'CON Please enter set your PIN:';
+
+            } else if ($level == 3) {
+                echo 'CON Please re-enter your PIN'; 
+
+            } else if ($level == 4) {
+                $name = $textArray[1];
+                $pin = $textArray[2];
+                $pin2 = $textArray[3];
+
+                if ($pin != $pin2) {
+                    echo 'END Your pins do not match, Try again';
+                } else {
+                    //register user in the database;
+                    //Send an SMS;
+                    echo 'END  Dear '. $name . ' You have successfully been registered';
+                }
+            }
+
+
+            // $message =  "END Your number has been recorded, we would get back to you";
+            // echo $message;
         }
         
         public function subMenuUnRegisteredTwo(){
@@ -33,7 +57,9 @@
             echo $message;
         }
 
-
+        public function sendMoneyMenu () {}
+        public function withdrawMoneyMenu () {}
+        public function checkBalanceMenu () {}
 
 
 
